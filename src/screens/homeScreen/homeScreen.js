75b92648bf1f549bda18b5f428c12d87ab68e8e3
Alexander <mnkwork3@gmail.com>
2022-02-17 11:28:00 +0700

    fix Home Screen

--- src/screens/homeScreen/homeScreen.js
+++ src/screens/homeScreen/homeScreen.js
@@ -2,35 +2,39 @@ import * as React from 'react';
 import { View,
         TouchableOpacity,
         Image,
-        FlatList} from 'react-native';
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+        FlatList,
+        ScrollView } from 'react-native';
 import {useEffect, useState} from "react";
 
 const HomeScreen = ({ navigation }) => {
-    let [images, setImage] = useState(undefined)
-    useEffect( () => {[
-        setImage(require('../../assets/imageMock/photo_2022-01-04_14-04-49.jpg')),
-        setImage(require('../../assets/imageMock/photo_2022-02-08_21-26-28.jpg')),
-        setImage(require('../../assets/imageMock/photo_2022-02-08_21-26-38.jpg')),
-        setImage(require('../../assets/imageMock/photo_2022-02-13_09-18-20.jpg'))
-    ]
-    })
+    const [images, setImage] = useState([
+        require('../../assets/imageMock/photo_2022-01-04_14-04-49.jpg'),
+        require('../../assets/imageMock/photo_2022-02-08_21-26-28.jpg'),
+        require('../../assets/imageMock/photo_2022-02-08_21-26-38.jpg'),
+        require('../../assets/imageMock/photo_2022-02-13_09-18-20.jpg')])
+
+
     const onPress = () => {
         navigation.navigate('Details')
     }
 
     return (
         <View style={{flex: 1, width: 640, height: 640}}>
-            <TouchableOpacity style={{flex: 1}} onPress={() => onPress()}>
-                <FlatList
-                    data={images}
-                    renderItem={({ item, index }) => (
-                        <Image source={item}
+            <ScrollView>
+                <TouchableOpacity style={{flex: 1}} onPress={() => onPress()}>
+                    <FlatList
+                        vertical={true}
+                        data={images}
+                        renderItem={({ item, index }) => (
+                            <Image
+                                style={{marginTop: 10,
+                                        marginBottom: 10}}
+                                source={item}
                                 key={index}/>
-                    )}
-                />
-            </TouchableOpacity>
+                        )}
+                    />
+                </TouchableOpacity>
+            </ScrollView>
         </View>
     )
 }
